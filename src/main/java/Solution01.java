ff83aadbe8d99e9269aab0fb9145dd6bb02810e6
Gabriel Martin <gabriel.martin1202@gmail.com>
2021-09-01 16:40:05 -0400

    Initial commit

---
+++ src/main/java/Solution01.java
@@ -0,0 +1,27 @@
+/*
+ * UCF COP3330 Fall 2021 Exercise 1 Solution
+ * Copyright 2021 Gabriel Martin
+ */
+
+/*
+ * import Scanner library
+ * create a new Scanner called input
+ * prompt the user to input their name
+ * 'name' = user input
+ * declare and set String 'output' = "hello" + 'name' + "nice to meet you"
+ * print 'output'
+ */
+
+import java.util.Scanner;
+
+public class Solution01 {
+    public static void main(String[] args) {
+        System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);         //Create Scanner objects
+
+        String name = input.nextLine();                 //Read user input
+        String output = "Hello, " + name + ", nice to meet you!";
+        System.out.println(output);
+
+    }
+}
